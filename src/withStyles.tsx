f50dd3524980316f635d6bd755b6f669c819cafa
McKayla Washburn <mckayla@hey.com>
2021-12-24 15:54:47 -0700

    doc comment

--- src/withStyles.tsx
+++ src/withStyles.tsx
@@ -2,6 +2,9 @@ import { ComponentType } from "react";
 
 import { Styles, Stylist } from "./Stylist";
 
+/**
+ * Utility function that wraps your component in a Stylist provider
+ */
 export function withStyles<Props>(styles: Styles, Component: ComponentType<Props>) {
 	return function (props: Props) {
 		return (
